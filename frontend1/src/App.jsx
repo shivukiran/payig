cbce30022519bc4b4d8cbab8a751cf446fbbb10d
shivukiran <ksvp1125@gmail.com>
2024-10-12 01:41:48 +0530

    added Frontend Basics

---
+++ frontend1/src/App.jsx
@@ -0,0 +1,27 @@
+import {
+  BrowserRouter,
+  Route,
+  Routes,
+} from "react-router-dom";
+import { Signup } from "./pages/signup";
+import { Signin } from "./pages/signin";
+// import { Dashboard } from "./pages/Dashboard";
+// import { SendMoney } from "./pages/SendMoney";
+
+function App() {
+  return (
+    <>
+       <BrowserRouter>
+        <Routes>
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/signin" element={<Signin />} />
+          {/* <Route path="/dashboard" element={<Dashboard />} /> */}
+          {/* <Route path="/send" element={<SendMoney />} /> */}
+        </Routes>
+      </BrowserRouter>
+    </>
+  )
+}
+
+
+export default App
